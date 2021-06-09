59179c967e66ca16bd8463c3ccbb4b5b2dfec0c1
okan çivgin <okanbjkspd@hotmail.com>
2021-06-10 00:27:06 +0300

    12 ders sonu

--- src/services/productService.js
+++ src/services/productService.js
@@ -7,5 +7,7 @@ export default class ProductService{
 
      getByProductName(productName){
         return axios.get("http://localhost:8080/api/products/getByProductName?productName="+ productName)
+
+        
     }
 }
